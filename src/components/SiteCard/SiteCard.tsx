3fdb61332d38a852683025507faf34dd6f9f4ca3
Naitchi <assasindu39@gmail.com>
2024-02-07 16:03:16 +0100

    ✨ Created SiteCard component

---
+++ src/components/SiteCard/SiteCard.tsx
@@ -0,0 +1,33 @@
+// Import React/Redux
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+// Style
+import styles from './SiteCard.module.scss';
+
+type SiteCardProps = {
+  image: string;
+  url: string;
+  desc: string;
+};
+
+const SiteCard: React.FC<SiteCardProps> = ({ image, url, desc }) => {
+  const [hover, setHover] = useState(false);
+  return (
+    <button
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className={styles.siteCard}
+      style={{ backgroundImage: `url(/assets/${image})` }}
+    >
+      <div className={`${hover ? styles.hover : ''} ${styles.content}`}>
+        <p className={styles.desc}>{desc}</p>
+        <Link className={styles.btn} href={url}>
+          Visiter le site
+        </Link>
+      </div>
+    </button>
+  );
+};
+
+export default SiteCard;
