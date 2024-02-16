4eb146e9062a5ceb4b9c80b7a70204d3f1b071fa
Naitchi <assasindu39@gmail.com>
2024-02-16 18:50:31 +0100

    ✨ added link to linkedin

--- src/components/Recommandations/Recommandations.tsx
+++ src/components/Recommandations/Recommandations.tsx
@@ -7,6 +7,11 @@ import styles from './Recommandations.module.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+// Import Fontawesome
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
 export default function Recommandations() {
   const settings = {
     dots: true,
@@ -21,23 +26,41 @@ export default function Recommandations() {
       <div className={styles.reco}>
         <Slider {...settings} className={styles.slider}>
           <div className={styles.slide}>
-            <img className={styles.img} src="/assets/ManuPP.webp" alt="Photo de Manuel" />
+            <img className={styles.img} src="/assets/ManuPP.jpg" alt="Photo de Manuel" />
             <h3 className={styles.name}>Manuel Trambert</h3>
             <p className={styles.work}>Ancien Lead Développeur ches Mes-Allocs</p>
+            <a
+              href="https://www.linkedin.com/in/manuel-trambert-731312a9/"
+              className={styles.linkedin}
+            >
+              <FontAwesomeIcon className={styles.icon} icon={faLinkedin} size="xl" />
+              Profil LinkedIn
+            </a>
           </div>
           <div className={styles.slide}>
             <img
               className={styles.img}
-              src="/assets/JoffreyPP.webp"
+              src="/assets/JoffreyPP.jpg"
               alt="Photo représentant Joffrey"
             />
             <h3 className={styles.name}>Joffrey Hernandez</h3>
             <p className={styles.work}>Développeur Web et Mentor chez OpenClassrooms</p>
+            <a href="https://www.linkedin.com/in/joffreyhernandez/" className={styles.linkedin}>
+              <FontAwesomeIcon className={styles.icon} icon={faLinkedin} size="xl" />
+              Profil LinkedIn
+            </a>
           </div>
           <div className={styles.slide}>
-            <img className={styles.img} src="/assets/PwccaPP.PNG" alt="Photo de Pwcca" />
+            <img className={styles.img} src="/assets/PwccaPP.jpg" alt="Photo de Pwcca" />
             <h3 className={styles.name}>Dylan &quot;Pwcca&quot; Bouvier</h3>
             <p className={styles.work}>Directeur de Production chez Solary</p>
+            <a
+              href="https://www.linkedin.com/in/dylan-bouvier-9956a219b/"
+              className={styles.linkedin}
+            >
+              <FontAwesomeIcon className={styles.icon} icon={faLinkedin} size="xl" />
+              Profil LinkedIn
+            </a>
           </div>
         </Slider>
       </div>
